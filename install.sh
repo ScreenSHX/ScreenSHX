f49ceec1cd539967e011d541eaf70f96fd3a7d25
Michael <identificationsucks@gmail.com>
2024-06-24 09:08:09 +1000

    Revice installed on SH.

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
+#!/bin/bash
+
 echo "Installing Yarn globally..."
-npm -ig yarn &> /dev/null
+npm install -g yarn
+if [ $? -eq 0 ]; then
+    echo "Yarn installed successfully."
+else
+    echo "Failed to install Yarn."
+    exit 1
+fi
 
 echo "Installing Electron and node-notifier using Yarn..."
 yarn add electron node-notifier
+if [ $? -eq 0 ]; then
+    echo "Electron and node-notifier installed successfully."
+else
+    echo "Failed to install Electron and node-notifier."
+    exit 1
+fi
+
+echo "All installations completed successfully."
+exit 0
+
